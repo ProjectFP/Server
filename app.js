3dfe8ddbc15c892ac85bc453c9520f0df5e419c9
Joe Dou <jdou@crittercism.com>
2015-02-06 00:26:27 -0800

    implement the function to upload image and saving it to disk

---
+++ app.js
@@ -0,0 +1,10 @@
+// require app-config file for the server
+var app = require('./lib/routes.js');
+
+//set port to 3000
+app.set('port', process.env.PORT || 3000);
+
+// listen at port and log port #
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
