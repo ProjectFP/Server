a32dc846699ececbf3432b51566237e24a10179b
Joe Dou <jdou@crittercism.com>
2015-06-13 10:16:49 -0700

    json webtokens and update mongo schema

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ var cors       = require('cors');
 var routes     = require('./lib/interfaces/rest/v1.0/routes');
 var qt         = require('quickthumb');
 var bodyParser = require('body-parser');
+var jwt        = require('express-jwt');
+var fs         = require('fs');
+var publicKey  = fs.readFileSync('/pat/to/public.pub');
 
 var app = express();
 app.use(cors());
 app.use(qt.static(__dirname + '/'));
 app.use(bodyParser());
+app.use(jwt({ 
+  secret: publicKey
+}).unless({path: ['/auth']}));
 
 routes.connectRoutes(app);
 
