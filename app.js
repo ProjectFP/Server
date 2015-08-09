51811b94d6422d4295d79cad191e8112f5e2de4e
Joe Dou <jdou@crittercism.com>
2015-08-08 23:06:41 -0700

    consolidate login and register

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(expressJwt({ 
     secret: config.SUPER_SECRET
-}).unless({path: ['/login', '/signup']})); 
+}).unless({path: ['/login', '/token']})); 
 
 routes.connectRoutes(app);
 
